04496383807f5108115dc7355ed48a194d92a3d4
choisungki <florist.sk@gmail.com>
2026-03-06 13:11:30 +0900

    v0.0.1

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { calculateStreak } from "./streak.js";
 import { getTheme } from "./colors.js";
 import { renderGraph } from "./render.js";
 
-const VERSION = "0.1.0";
+const VERSION = "0.0.1";
 
 interface Options {
   username: string;
